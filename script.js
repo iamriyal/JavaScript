45608f993aac5d92b1209a6d862f791340faec2b
Riyal Rane <rane.riyal03@gmail.com>
2024-01-31 01:00:43 +0530

    javascript basics

--- script.js
+++ script.js
@@ -12,13 +12,33 @@
 
 // Strings
 
-let fname="Riyal"
-let lname="Rane"
-console.log(fname,lname)
-console.log(fname+lname)
+// let fname="Riyal"
+// let lname="Rane"
+// console.log(fname,lname)
+// console.log(fname+lname)
 
-let fullname=`hello My first name is ${fname} and my last name is ${lname}`
+// let fullname=`hello My first name is ${fname} and my last name is ${lname}`
 
-console.log(fullname)
+// console.log(fullname)
 
 
+// console.log(fname.toUpperCase())
+
+
+// let str="           hello my name is Ram               "
+// console.log(str.trim())
+// let result=str.trim()
+// console.log(result.includes('hello'))
+
+// // .trim() removes unnecessary spaces
+// console.log(`The OG String is ${result}`)
+
+// let favcolors="Blue Brown Black Yellow"
+// let arrcolors=favcolors.split(' ')
+// console.log(arrcolors)
+
+// let age=854
+// console.log(age=="854") //checks only for value and not for type
+// console.log(age===854) //checks for both value and type
+
+// 48.15 30-01-2024
