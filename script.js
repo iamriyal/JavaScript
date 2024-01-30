451259ca8fd1e30d9223b5a28968aa8c2f659f13
Riyal Rane <rane.riyal03@gmail.com>
2024-01-31 00:25:18 +0530

    javascript basics

---
+++ script.js
@@ -0,0 +1,24 @@
+// // Variable Constants And Comments
+
+// var score=10
+// var score=54
+// console.log(score)
+
+
+// // Constant
+// const pi=3.14
+// console.log(pi)
+
+
+// Strings
+
+let fname="Riyal"
+let lname="Rane"
+console.log(fname,lname)
+console.log(fname+lname)
+
+let fullname=`hello My first name is ${fname} and my last name is ${lname}`
+
+console.log(fullname)
+
+
